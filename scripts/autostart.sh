c29e51b0f0fa183def0d310792beeb0c077a726b
nikmosi <i@nikmosi.ru>
2024-03-15 19:25:05 +0700

    feat(autostart): add prismatik

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,4 +5,4 @@ greenclip daemon &
 setxkbmap -option 'grp:alt_shift_toggle' -layout us,ru
 # for id in $(xinput list | grep "pointer" | cut -d '=' -f 2 | cut -f 1); do xinput --set-prop $id 'libinput Accel Profile Enabled' 0, 1; done
 chatterino &
-prismatik &
+(prismatik && prismatik --on) &
