17b901284784a56d3790607df77d654f2decb9c7
nikmosi <i@nikmosi.ru>
2025-01-26 22:27:27 +0700

    change order

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 chatterino &
+ayugram-desktop &
 nekoray &
-prismatik &; echo (sleep 0.5 && prismatik --on && prismatik --on) &
 tmux start-server &
+prismatik &; echo (sleep 0.5 && prismatik --on && prismatik --on) &
