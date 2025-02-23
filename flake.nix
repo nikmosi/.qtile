61afca6592f91a5df8518503fb26cd505b399006
nikmosi <i@nikmosi.ru>
2025-02-23 15:48:59 +0700

    add git-hooks.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,46 @@
 {
-  description = "A very basic flake";
+  description = "An example project.";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.pre-commit-hooks.url = "github:cachix/git-hooks.nix";
 
   outputs =
-    { self, nixpkgs }:
+    { self, nixpkgs, ... }@inputs:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
+      checks = forAllSystems (system: {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            isort.enable = true;
+            ruff.enable = true;
+            ruff-format.enable = true;
+            end-of-file-fixer.enable = true;
+            check-added-large-files.enable = true;
+            trim-trailing-whitespace.enable = true;
+            check-yaml.enable = true;
+            fix-byte-order-marker.enable = true;
+            trufflehog.enable = true;
+          };
+        };
+      });
 
-      devShells.${system}.default = import ./shell.nix { inherit pkgs; };
+      devShells = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+          shellHook = ''
+            ${self.checks.${system}.pre-commit-check.shellHook}
+            exec fish
+          '';
+        };
+      });
     };
 }
