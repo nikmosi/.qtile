12ea45a9e91dfe1081606760c04e6a4adfea25e0
nikmosi <i@nikmosi.ru>
2025-03-11 22:16:37 +0700

    remove gptcommit

--- flake.nix
+++ flake.nix
@@ -30,7 +30,6 @@
             check-yaml.enable = true;
             fix-byte-order-marker.enable = true;
             trufflehog.enable = true;
-            gptcommit.enable = true;
           };
         };
       });
